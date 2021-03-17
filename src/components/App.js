5c5f30a5ca46ee119d09b1c1bb4e5e18f22e1667
MetroICTDev <joshuabayo88@gmail.com>
2021-03-17 10:00:55 +0100

    keeper app initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* ES6 Anonymous Function */}
+      {notes.map((notes) =>
+      <Note
+        key={notes.id}
+        title={notes.title}
+        content={notes.content}
+         />
+  )}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
